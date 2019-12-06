3392d849450298abc73ce0316fd3a51058c8a001
Matt Porter <matthew.js.porter@gmail.com>
2019-12-05 22:25:34 -0500

    day 3, puzzle 1

---
+++ day3/ClosestIntersectionFinder.py
@@ -0,0 +1,63 @@
+class ClosestIntersectionFinder:
+    def __init__(self, rows, cols, central_port):
+        self.wire1 = self.__init_wire(cols, rows)
+        self.wire2 = self.__init_wire(cols, rows)
+        self.central_port = central_port
+        self.intersections = []
+
+    def __init_wire(self, cols, rows):
+        return [[0 for i in range(cols)] for j in range(rows)]
+
+    def draw_wire1(self, path):
+        self.__draw_wire(self.wire1, path)
+
+    def draw_wire2(self, path):
+        self.__draw_wire(self.wire2, path)
+
+    def calculate_intersections(self):
+        for i in range(0, len(self.wire1)):
+            for j in range(0, len(self.wire1[i])):
+                if self.wire1[i][j] == 1 and self.wire2[i][j] == 1:
+                    self.intersections.append((i, j))
+
+    def calculate_manhattan_distance(self, index1, index2):
+        return abs(index1[0] - index2[0]) + abs(index1[1] - index2[1])
+
+    def calculate_shortest_distance(self):
+        self.calculate_intersections()
+        shortest_intersection = self.calculate_manhattan_distance(self.central_port, self.intersections[0])
+        for intersection in self.intersections:
+            distance = self.calculate_manhattan_distance(self.central_port, intersection)
+            if distance < shortest_intersection:
+                shortest_intersection = distance
+        return shortest_intersection
+
+    def __draw_wire(self, wire, path):
+        index = self.central_port
+        for operation in path:
+            direction = operation[0]
+            length = int(operation[1:])
+            for i in range(length, 0, -1):
+                if direction == "R":
+                    index = (index[0], index[1] + 1)
+                elif direction == "U":
+                    index = (index[0]-1, index[1])
+                elif direction == "D":
+                    index = (index[0]+1, index[1])
+                else:
+                    index = (index[0], index[1] - 1)
+                wire[index[0]][index[1]] = 1
+
+
+if __name__ == '__main__':
+   closest_intersection_finder = ClosestIntersectionFinder(20000, 20000, (10000, 10000))
+   f = open("input.txt")
+   wire1_path = f.readline().split(",")
+   wire2_path = f.readline().split(",")
+   f.close()
+   closest_intersection_finder.draw_wire1(wire1_path)
+   closest_intersection_finder.draw_wire2(wire2_path)
+   print closest_intersection_finder.calculate_shortest_distance()
+
+
+
---
+++ day3/ClosestIntersectionFinderTest.py
@@ -0,0 +1,198 @@
+import unittest
+
+from day3.ClosestIntersectionFinder import ClosestIntersectionFinder
+
+
+class MyTestCase(unittest.TestCase):
+    def test_init_grid(self):
+        expected_grid = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+        intersection_finder = ClosestIntersectionFinder(4, 4, (0,4))
+
+        self.assertEqual(expected_grid, intersection_finder.wire1)
+        self.assertEqual(expected_grid, intersection_finder.wire2)
+        self.assertEqual(intersection_finder.central_port, (0,4))
+
+    def test_draw_wire1_right(self):
+        expected_wire1= [[0, 0, 0, 0],
+                         [0, 1, 1, 0],
+                         [0, 0, 0, 0],
+                         [0, 0, 0, 0]]
+
+        expected_wire2 = [[0, 0, 0, 0],
+                          [0, 0, 0, 0],
+                          [0, 0, 0, 0],
+                          [0, 0, 0, 0]]
+        intersection_finder = ClosestIntersectionFinder(4, 4, (1, 0))
+        intersection_finder.draw_wire1(["R2"])
+
+        self.assertEqual(expected_wire1, intersection_finder.wire1)
+        self.assertEqual(expected_wire2, intersection_finder.wire2)
+
+    def test_draw_wire2_right(self):
+        expected_wire2= [[0, 0, 0, 0],
+                         [0, 1, 1, 0],
+                         [0, 0, 0, 0],
+                         [0, 0, 0, 0]]
+
+        expected_wire1 = [[0, 0, 0, 0],
+                          [0, 0, 0, 0],
+                          [0, 0, 0, 0],
+                          [0, 0, 0, 0]]
+        intersection_finder = ClosestIntersectionFinder(4, 4, (1, 0))
+        intersection_finder.draw_wire2(["R2"])
+
+        self.assertEqual(expected_wire1, intersection_finder.wire1)
+        self.assertEqual(expected_wire2, intersection_finder.wire2)
+
+    def test_draw_wire1_left(self):
+        expected_wire1= [[0, 0, 0, 0],
+                         [0, 1, 1, 0],
+                         [0, 0, 0, 0],
+                         [0, 0, 0, 0]]
+
+        expected_wire2 = [[0, 0, 0, 0],
+                          [0, 0, 0, 0],
+                          [0, 0, 0, 0],
+                          [0, 0, 0, 0]]
+        intersection_finder = ClosestIntersectionFinder(4, 4, (1, 3))
+        intersection_finder.draw_wire1(["L2"])
+
+        self.assertEqual(expected_wire1, intersection_finder.wire1)
+        self.assertEqual(expected_wire2, intersection_finder.wire2)
+
+
+    def test_draw_wire2_left(self):
+        expected_wire2= [[0, 0, 0, 0],
+                         [0, 1, 1, 0],
+                         [0, 0, 0, 0],
+                         [0, 0, 0, 0]]
+
+        expected_wire1 = [[0, 0, 0, 0],
+                          [0, 0, 0, 0],
+                          [0, 0, 0, 0],
+                          [0, 0, 0, 0]]
+        intersection_finder = ClosestIntersectionFinder(4, 4, (1, 3))
+        intersection_finder.draw_wire2(["L2"])
+
+        self.assertEqual(expected_wire1, intersection_finder.wire1)
+        self.assertEqual(expected_wire2, intersection_finder.wire2)
+
+    def test_draw_wire1_up(self):
+        expected_wire1= [[0, 0, 0, 0],
+                         [1, 0, 0, 0],
+                         [1, 0, 0, 0],
+                         [0, 0, 0, 0]]
+
+        expected_wire2 = [[0, 0, 0, 0],
+                          [0, 0, 0, 0],
+                          [0, 0, 0, 0],
+                          [0, 0, 0, 0]]
+        intersection_finder = ClosestIntersectionFinder(4, 4, (3, 0))
+        intersection_finder.draw_wire1(["U2"])
+
+        self.assertEqual(expected_wire1, intersection_finder.wire1)
+        self.assertEqual(expected_wire2, intersection_finder.wire2)
+
+    def test_draw_wire2_up(self):
+        expected_wire2= [[0, 0, 0, 0],
+                         [1, 0, 0, 0],
+                         [1, 0, 0, 0],
+                         [0, 0, 0, 0]]
+
+        expected_wire1 = [[0, 0, 0, 0],
+                          [0, 0, 0, 0],
+                          [0, 0, 0, 0],
+                          [0, 0, 0, 0]]
+        intersection_finder = ClosestIntersectionFinder(4, 4, (3, 0))
+        intersection_finder.draw_wire2(["U2"])
+
+        self.assertEqual(expected_wire1, intersection_finder.wire1)
+        self.assertEqual(expected_wire2, intersection_finder.wire2)
+
+    def test_draw_wire1_down(self):
+        expected_wire1 = [[0, 0, 0, 0],
+                          [1, 0, 0, 0],
+                          [1, 0, 0, 0],
+                          [0, 0, 0, 0]]
+
+        expected_wire2 = [[0, 0, 0, 0],
+                          [0, 0, 0, 0],
+                          [0, 0, 0, 0],
+                          [0, 0, 0, 0]]
+        intersection_finder = ClosestIntersectionFinder(4, 4, (0, 0))
+        intersection_finder.draw_wire1(["D2"])
+
+        self.assertEqual(expected_wire1, intersection_finder.wire1)
+        self.assertEqual(expected_wire2, intersection_finder.wire2)
+
+    def test_draw_wire2_down(self):
+        expected_wire2 = [[0, 0, 0, 0],
+                          [1, 0, 0, 0],
+                          [1, 0, 0, 0],
+                          [0, 0, 0, 0]]
+
+        expected_wire1 = [[0, 0, 0, 0],
+                          [0, 0, 0, 0],
+                          [0, 0, 0, 0],
+                          [0, 0, 0, 0]]
+        intersection_finder = ClosestIntersectionFinder(4, 4, (0, 0))
+        intersection_finder.draw_wire2(["D2"])
+
+        self.assertEqual(expected_wire1, intersection_finder.wire1)
+        self.assertEqual(expected_wire2, intersection_finder.wire2)
+
+    def test_draw_multi(self):
+        expected_wire1 = [[0, 0, 0, 0],
+                          [1, 0, 0, 0],
+                          [1, 1, 1, 0],
+                          [0, 0, 0, 0]]
+
+        intersection_finder = ClosestIntersectionFinder(4, 4, (0, 0))
+        intersection_finder.draw_wire1(["D2", "R2"])
+
+        self.assertEqual(expected_wire1, intersection_finder.wire1)
+
+    def test_get_intersections(self):
+        intersection_finder = ClosestIntersectionFinder(4, 4, (0, 0))
+        intersection_finder.wire1 = [[1, 1, 1, 1],
+                          [0, 0, 0, 0],
+                          [1, 1, 1, 1],
+                          [0, 0, 0, 0]]
+
+        intersection_finder.wire2 = [[1, 0, 0, 0],
+                          [1, 0, 0, 0],
+                          [1, 0, 0, 0],
+                          [1, 0, 0, 0]]
+
+        expected_intersections = [(0,0), (2,0)]
+        intersection_finder.calculate_intersections()
+        self.assertEqual(expected_intersections, intersection_finder.intersections)
+
+
+    def test_get_manhattan_distance(self):
+        intersection_finder = ClosestIntersectionFinder(10, 10, (8, 1))
+        distance = intersection_finder.calculate_manhattan_distance((8, 1), (5, 4))
+        expected_distance = 6
+        self.assertEqual(expected_distance, distance)
+
+    def test_get_shortest_distance(self):
+        intersection_finder = ClosestIntersectionFinder(1000, 1000, (8, 1))
+        intersection_finder.draw_wire1(["R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"])
+        intersection_finder.draw_wire2(["U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"])
+        expected_distance = 159
+        distance = intersection_finder.calculate_shortest_distance()
+        self.assertEqual(expected_distance, distance)
+
+    def test_get_shortest_distance_example_2(self):
+        intersection_finder = ClosestIntersectionFinder(1000, 1000, (500, 500))
+        intersection_finder.draw_wire1(["R98", "U47", "R26", "D63", "R33", "U87", "L62", "D20", "R33", "U53", "R51"])
+        intersection_finder.draw_wire2(["U98", "R91", "D20", "R16", "D67", "R40", "U7", "R15", "U6", "R7"])
+        expected_distance = 135
+        distance = intersection_finder.calculate_shortest_distance()
+        self.assertEqual(expected_distance, distance)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
