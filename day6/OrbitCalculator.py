a7893e869de03230854dcb1def15aaea31a1cb18
Matt Porter <matthew.js.porter@gmail.com>
2019-12-10 21:48:05 -0500

    day 6, puzzle 2

--- day6/OrbitCalculator.py
+++ day6/OrbitCalculator.py
@@ -21,6 +21,47 @@ class OrbitCalculator:
                 number_of_orbits += self.calc_number_of_orbits(o, depth + 1)
             return depth + number_of_orbits
 
+    def get_paths(self, center_of_mass, dest):
+        if center_of_mass.name == dest:
+            return [center_of_mass.name]
+        else:
+            paths = []
+            for o in center_of_mass.nodes:
+                node_paths = self.get_paths(o, dest)
+                if len(node_paths) > 0:
+                    if len(node_paths) > 0 and isinstance(node_paths[0], list):
+                        paths = [[center_of_mass.name] + p for p in node_paths]
+                    elif len(paths) == 0:
+                        paths = [center_of_mass.name] + node_paths
+                    else:
+                        node_paths = [center_of_mass.name] + node_paths
+                        paths = [paths] + [node_paths]
+            return paths
+
+    def get_min_orbital_transfers(self, orbit, dest1, dest2):
+        dest1_paths = self.get_paths(orbit, dest1)
+        dest2_paths = self.get_paths(orbit, dest2)
+
+        if not isinstance(dest1_paths[0], list):
+            dest1_paths = [dest1_paths]
+        if not isinstance(dest2_paths[0], list):
+            dest2_paths = [dest2_paths]
+
+        min_distance = len(dest1_paths[0]) + len(dest2_paths[0])
+        for dest1_path in dest1_paths:
+            for dest2_path in dest2_paths:
+                for i in range(len(dest1_path) - 1, -1, -1):
+                    for j in range(len(dest2_path) - 1, -1, -1):
+                        if dest1_path[i] == dest2_path[j]:
+                            distance1 = len(dest1_path) - i - 1
+                            distance2 = len(dest2_path) - j - 1
+                            distance = distance1 + distance2
+                            if distance < min_distance:
+                                min_distance = distance
+
+        return min_distance
+
+
 
 if __name__ == '__main__':
     f = open("input.txt")
@@ -40,4 +81,7 @@ if __name__ == '__main__':
             nodes[name] = Orbit(name, [child_orbit])
 
     orbitCalculator = OrbitCalculator()
-    print orbitCalculator.calculate_number_of_orbits(nodes["COM"])
+
+    san_orbit = orbitCalculator.get_paths(nodes["COM"], "SAN")[-2]
+    you_orbit = orbitCalculator.get_paths(nodes["COM"], "YOU")[-2]
+    print orbitCalculator.get_min_orbital_transfers(nodes["COM"], san_orbit, you_orbit)
--- day6/OrbitCalculatorTest.py
+++ day6/OrbitCalculatorTest.py
@@ -35,6 +35,84 @@ class MyTestCase(unittest.TestCase):
         orbitCalculator = OrbitCalculator()
         self.assertEqual(42, orbitCalculator.calculate_number_of_orbits(orbit))
 
+    def test_calculate_paths(self):
+        orbit = Orbit("COM")
+        orbit.nodes = [
+            Orbit("B", [
+                Orbit("G", [
+                    Orbit("H")
+                ]),
+                Orbit("C", [
+                    Orbit("D", [
+                        Orbit("I"),
+                        Orbit("E", [
+                            Orbit("F"),
+                            Orbit("J", [
+                                Orbit("K", [
+                                    Orbit("L")
+                                ])
+                            ])
+                        ])
+                    ])
+                ])
+            ])
+        ]
+        expected_path = ["COM", "B", "C", "D", "I"]
+        orbitCalculator = OrbitCalculator()
+        self.assertEqual(expected_path, orbitCalculator.get_paths(orbit, "I"))
+
+
+    def test_calculate_multiple_paths(self):
+        orbit = Orbit("COM")
+        orbit.nodes = [
+            Orbit("B", [
+                Orbit("G", [
+                    Orbit("I")
+                ]),
+                Orbit("C", [
+                    Orbit("D", [
+                        Orbit("I"),
+                        Orbit("E", [
+                            Orbit("F"),
+                            Orbit("J", [
+                                Orbit("K", [
+                                    Orbit("L")
+                                ])
+                            ])
+                        ])
+                    ])
+                ])
+            ])
+        ]
+        expected_path = [["COM", "B", "G", "I"], ["COM", "B", "C", "D", "I"]]
+        orbitCalculator = OrbitCalculator()
+        self.assertEqual(expected_path, orbitCalculator.get_paths(orbit, "I"))
+
+    def test_min_orbit(self):
+        orbit = Orbit("COM")
+        orbit.nodes = [
+            Orbit("B", [
+                Orbit("G", [
+                    Orbit("H")
+                ]),
+                Orbit("C", [
+                    Orbit("D", [
+                        Orbit("I"),
+                        Orbit("E", [
+                            Orbit("F"),
+                            Orbit("J", [
+                                Orbit("K", [
+                                    Orbit("L")
+                                ])
+                            ])
+                        ])
+                    ])
+                ])
+            ])
+        ]
+        orbitCalculator = OrbitCalculator()
+        self.assertEqual(4, orbitCalculator.get_min_orbital_transfers(orbit, "K", "I"))
+
 
 if __name__ == '__main__':
     unittest.main()
